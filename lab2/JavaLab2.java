a9bb2ef39f0ac9889b71967fe4db2819c64f3759
Harun Saib <144715661+Garnetic@users.noreply.github.com>
2024-04-24 22:03:17 -0700

    Update JavaLab2.java

--- lab2/JavaLab2.java
+++ lab2/JavaLab2.java
@@ -1,43 +1,27 @@
-import java.util.HashMap;
-
 public class JavaLab2 {
-    public class AnagramChecker {
+    public static void main(String[] args) {
+        String s = "listen";
+        String t = "silent";
 
-        public static boolean isAnagram(String s, String t) {
-            if (s.length() != t.length()) {
-                return false;
-            }
-
-            HashMap<Character, Integer> charCounts = new HashMap<>();
-
-            // Count each character in string s
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
-            }
-
-            // Reduce the count for each character found in t
-            for (int i = 0; i < t.length(); i++) {
-                char c = t.charAt(i);
-                if (!charCounts.containsKey(c) || charCounts.get(c) == 0) {
-                    return false;
-                }
-                charCounts.put(c, charCounts.get(c) - 1);
-                if (charCounts.get(c) == 0) {
-                    charCounts.remove(c);
-                }
-            }
-
-            // If the map is empty, then t is an anagram of s
-            return charCounts.isEmpty();
-        }
-
-        public static void main(String[] args) {
-            String s = "listen";
-            String t = "silent";
-
-            System.out.println("Is \"" + t + "\" an anagram of \"" + s + "\"? " + isAnagram(s, t));
-        }
+        System.out.println("Is \"" + t + "\" an anagram of \"" + s + "\"? " + isAnagram(s, t));
     }
 
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) { // If they are not the same length,
+            return false; // Then they're not an anagram
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (j == s.length() - 1 && s.charAt(i) != t.charAt(j)) {
+                    return false;
+                }
+                if (s.charAt(i) == t.charAt(j)) {
+                    break;
+                }
+            }
+        }
+
+        return true;
+    }
 }
